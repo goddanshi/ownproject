701f1cdfb88c841e0bd1f9694b28f1b0fd473c8f
goddanshi <oceanfluddy@gmail.com>
2025-10-13 07:11:22 +0300

    Add console.php

---
+++ backend/backend/config/console.php
@@ -0,0 +1,25 @@
+<?php
+$params = array_merge(
+    require __DIR__ . '/../../common/config/params.php',
+    require __DIR__ . '/../../common/config/params-local.php',
+    require __DIR__ . '/params.php',
+    require __DIR__ . '/params-local.php'
+);
+
+return [
+    'id' => 'app-backend-console',
+    'basePath' => dirname(__DIR__),
+    'controllerNamespace' => 'backend\commands',
+    'components' => [
+        'db' => require __DIR__ . '/db.php',
+        'log' => [
+            'targets' => [
+                [
+                    'class' => \yii\log\FileTarget::class,
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+    ],
+    'params' => $params,
+];
