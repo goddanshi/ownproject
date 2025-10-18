3a4a77285e792c53e65981c56accfa794fb6218e
goddanshi <oceanfluddy@gmail.com>
2025-10-18 22:19:22 +0300

    add cors errors fix

--- backend/backend/controllers/ApiController.php
+++ backend/backend/controllers/ApiController.php
@@ -19,7 +19,7 @@ class ApiController extends Controller
             'class' => \yii\filters\Cors::class,
             'cors' => [
                 'Origin' => [
-                    'http://81.19.136.133',
+                    'http://185.213.240.236',
                     'http://localhost:5173',
                     'http://127.0.0.1:5173'
                 ],
